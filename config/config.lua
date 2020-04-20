c60a519bdacbae361f351ec4e020bb143edd7910
Scullyy <51968381+Scullyy@users.noreply.github.com>
2020-04-20 03:47:28 +0100

    Fixed an idiot of a mistake

--- config/config.lua
+++ config/config.lua
@@ -4,7 +4,7 @@
 
 Config = {}
 
-Timer = {
+Config.Timer = {
     cooldown = 20, -- How long should the default timer be in minutes?
 	alertmessage = "~r~A robbery happened less than ~y~20 ~r~minutes ago, hold your horses!!", -- Message when someone tries to rob something while the cooldown is in effect.
 	endedmessage = "~g~The global timer has ended, feel free to rob some shit!" -- Message sent out to all players when the cooldown ends, set to "" to disable it.
