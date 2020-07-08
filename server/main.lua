0d600719811aff90f45b031b8209366b1ad71f22
Scullyy <51968381+Scullyy@users.noreply.github.com>
2020-07-08 01:57:25 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -6,40 +6,40 @@ local cooldown = false
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 ESX.RegisterServerCallback('Scully:CanStartRobbery', function(source, cb)
-	local xPlayer = ESX.GetPlayerFromId(source)
-	if xPlayer and not cooldown then
-	    cb(true)
-	else
-	    xPlayer.showNotification(Config.Timer.alertmessage)
-	    cb(false)
-	end
+    local xPlayer = ESX.GetPlayerFromId(source)
+    if xPlayer and not cooldown then
+	cb(true)
+    else
+	xPlayer.showNotification(Config.Timer.alertmessage)
+	cb(false)
+    end
 end)
 
 RegisterServerEvent("Scully:StartTimer")
 AddEventHandler("Scully:StartTimer", function(cooldowntime)
     local cooldowntimer = Config.Timer.cooldown
-	if cooldowntime ~= nil and cooldowntime > 0 then 
-		cooldowntimer = cooldowntime 
-	end
+    if cooldowntime ~= nil and cooldowntime > 0 then 
+	cooldowntimer = cooldowntime 
+    end
     cooldown = true
     SetTimeout((cooldowntimer*60000), function()
-		cooldown = false
-		TimerHasEnded()
-	end)
+	cooldown = false
+	TimerHasEnded()
+    end)
 end)
 
 RegisterServerEvent("Scully:ResetTimer")
 AddEventHandler("Scully:ResetTimer", function()
-	cooldown = false
-	TimerHasEnded()
+    cooldown = false
+    TimerHasEnded()
 end)
 
 function TimerHasEnded()
     local xPlayers = ESX.GetPlayers()
-	for i=1, #xPlayers, 1 do
-		local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
-		if Config.Timer.endedmessage ~= "" then
-		    xPlayer.showNotification(Config.Timer.endedmessage)
-		end
+    for i=1, #xPlayers, 1 do
+	local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+	if Config.Timer.endedmessage ~= "" then
+            xPlayer.showNotification(Config.Timer.endedmessage)
 	end
+    end
 end
