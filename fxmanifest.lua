4d4bb24db0edd72856df55b75f40ddd6505f539e
Scullyy <51968381+Scullyy@users.noreply.github.com>
2020-07-08 02:04:08 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,10 @@
 -- Created by Scully#5775 - https://discord.gg/eNtGFS6
 
-fx_version 'adamant'
+fx_version 'bodacious'
 
 game 'gta5'
+description 'Global bank robbery timer'
+version '1.1'
 
 server_scripts {
 	'config/config.lua',
